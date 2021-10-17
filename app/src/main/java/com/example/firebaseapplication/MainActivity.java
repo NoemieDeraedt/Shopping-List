d2c53086b9a9317f6a514704491678566b4abff1
Noémie <noemie.deraedt@epitech.eu>
2021-10-17 18:08:20 +0100

    feat: 🎸 checking requirements for password & username

--- app/src/main/java/com/example/firebaseapplication/MainActivity.java
+++ app/src/main/java/com/example/firebaseapplication/MainActivity.java
@@ -8,10 +8,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public EditText username;
+    public EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
     public void onClickSignInButton(View v) {
         username = (EditText)findViewById(R.id.username);
+        password = (EditText)findViewById(R.id.password);
 
-        if (username.getText().length() == 0) {
-
+        if (password.getText().length() < 6) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                "Your password needs to have more than 6 characters.",
+                Toast.LENGTH_SHORT);
+            toast.show();
+        } else if (username.getText().length() == 0) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Your username cannot be empty.",
+                    Toast.LENGTH_SHORT);
+            toast.show();
         } else {
             Intent i = new Intent(this, ShoppingList.class);
             i.putExtra("username", String.valueOf(username.getText()));
--- app/src/main/java/com/example/firebaseapplication/Register.java
+++ app/src/main/java/com/example/firebaseapplication/Register.java
@@ -5,8 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+    public EditText username;
+    public EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +25,23 @@ public class Register extends AppCompatActivity {
     }
 
     public void onClickRegisterButton(View v) {
-        Intent i = new Intent(this, ShoppingList.class);
-        startActivity(i);
+        username = (EditText)findViewById(R.id.usernameRegister);
+        password = (EditText)findViewById(R.id.passwordRegister);
+
+        if (password.getText().length() < 6) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Your password needs to have more than 6 characters.",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        } else if (username.getText().length() == 0) {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Your username cannot be empty.",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        } else {
+            Intent i = new Intent(this, ShoppingList.class);
+            i.putExtra("username", String.valueOf(username.getText()));
+            startActivity(i);
+        }
     }
 }
